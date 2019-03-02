180987e6b5759b2d7a01724d7cfbdb42bae4d2b6
Admin <Admin@PC>
2019-03-02 22:25:22 +0530

    local commitdemo repo

---
+++ DemoProj/src/com/demo/DemoRepo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class DemoRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello frmo demo repo");
+	}
+
+}
